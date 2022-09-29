e42d0dc0126ace63ec3a63f8e4500738d3a187a4
Pedro Solorzano <pedro1991luis@gmail.com>
2022-09-28 22:43:01 -0500

    primer pase de la lading 28-09

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -3,7 +3,60 @@ import '../styles/Formulario.css';
 
 const Formulario = () => {
     return (
-        <div>Formulario</div>
+        <div className="contenedor-formulario">
+            <h2>Ingresa tus datos y te llamaremos.</h2>
+            <form>
+            <div className="content-campos-form">
+                <input type="text" name="myname[0]" />
+                <label>
+                    Nombres y apellidos
+                </label>
+                <span className="boder-form"></span>
+            </div>
+
+            <div className="content-campos-form">
+                <input type="number" name="myname[1]" />
+                <label>
+                    Número de documento de identidad
+                </label>
+                <span className="boder-form"></span>
+            </div>
+
+            <div className="content-campos-form">
+                <input type="number" name="myname[2]" />
+                <label>
+                    Número de celular
+                </label>
+            <span className="boder-form"></span>
+            </div>
+
+            <div className="content-campos-form">
+                <input type="mail" name="myname[3]" />
+                <label>
+                    Correo electrónico
+                </label>
+                <span className="boder-form"></span>
+            </div>
+            <div className="content-campos-form">
+                <select name="myname[4]" id="">
+                    <option value="" name="myname[4]">¿Eres cliente de Claro?</option>
+                    <option value="Si" name="myname[4]">Si</option>
+                    <option value="No" name="myname[4]">No</option>
+                </select>
+                <span className="boder-form"></span>
+            </div>
+            <div className="content-campos-form">
+                <input type="checkbox" name="myname[6]"/>
+                <div>Autorizo el <a href="#">Tratamiento de datos personales</a></div>
+            </div>
+            <div className="content-campos-form">
+                <input type="checkbox" name="myname[6]"/>
+                <div>Acepto los términos y condiciones.</div>
+            </div>
+            <div>
+                <button type="submit" value="Te llamamos" className="btn-enviar">Te llamamos</button></div>
+            </form>
+        </div>
     );
 }
 
--- src/containers/Banner.jsx
+++ src/containers/Banner.jsx
@@ -4,12 +4,22 @@ import '../styles/Banner.css';
 
 const Banner = () => {
     return (
-        <div>
+        <div className="banner-content">
+            <div className="content-banner-ppal">
+            <div className="content-img-banner">
+                <img className="logo-banco" src="https://static.claro.com.pe/img/otros/logo-bbva-12-08.png" alt="" />
+                <img className="img-text" src="https://static.claro.com.pe/img/otros/Web-bbva-15-09.png" alt="" />
+                <img className="img-segundaria" src="https://static.claro.com.pe/img/otros/2-bbva-13-09.png" alt="" />
             
-            <div><p>imagenes</p></div>
+            
+            </div>
             <Formulario />
-            <h1>Exclusivo con tu tarjeta de crédito BBVA Visa o Mastercard.</h1>
+            </div>
+            <div className="content-text-bottom">
+                <h1>Exclusivo con tu tarjeta de crédito BBVA Visa o Mastercard.</h1>
+            </div>
         </div>
+        
 
     );
 }
